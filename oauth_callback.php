db22d832b5e6f5a1d5426788d70202f283ac2a82
Sai Vignesh <suramsaivignesh@gmail.com>
2026-02-18 16:18:22 +0530

    changes done

--- oauth_callback.php
+++ oauth_callback.php
@@ -2,15 +2,9 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 $client = new Google\Client();
-
-// credentials from env
-$client->setAuthConfig(
-    json_decode($_ENV['GOOGLE_CREDENTIALS'], true)
-);
-
-// 🔴 ADD THIS LINE HERE
-$client->setRedirectUri($_ENV['GOOGLE_REDIRECT_URI']);
-
+$client->setClientId($_ENV['GOOGLE_CLIENT_ID'] ?? getenv('GOOGLE_CLIENT_ID'));
+$client->setClientSecret($_ENV['GOOGLE_CLIENT_SECRET'] ?? getenv('GOOGLE_CLIENT_SECRET'));
+$client->setRedirectUri($_ENV['GOOGLE_REDIRECT_URI'] ?? getenv('GOOGLE_REDIRECT_URI'));
 $client->setScopes(Google\Service\Drive::DRIVE_FILE);
 $client->setAccessType('offline');
 $client->setPrompt('consent');
@@ -22,7 +16,7 @@ if (!isset($_GET['code'])) {
 $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
 
 if (isset($token['error'])) {
-    exit('OAuth error');
+    exit('OAuth error: ' . $token['error_description']);
 }
 
 file_put_contents(__DIR__ . '/token.json', json_encode($token));
